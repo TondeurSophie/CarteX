2647edb36bf41ff569c7757f9b81bf338379ae60
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-12-18 14:08:31 +0100

    feat Controller : back connexion inscription
    back des pages de connexion et d'inscription

---
+++ src/API.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const serveur = require('./Serveur');
+
+console.log("Ceci est l'api.");
+
+// Démarrage du serveur puis exécution du code API
+serveur.startServer().then(() => {
+    console.log("Ceci est l'api.");
+
+    http.get('http://localhost:3008/api/cartes', (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            console.log(data);
+        });
+
+    }).on('error', (err) => {
+        console.error('Error: ' + err.message);
+    });
+});
