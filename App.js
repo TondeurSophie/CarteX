d4c0cb0f114f68f625d1bf30031cb7191c1c5427
TondeurSophie <149052701+TondeurSophie@users.noreply.github.com>
2023-12-18 15:34:44 +0100

    feat controller : ajout redirection
     redirection vers page profil.

---
+++ App.js
@@ -0,0 +1,36 @@
+import './App.css'; //relie cette page au css App.css
+import { Routes, Route, Link } from 'react-router-dom'; //permet l'utilisation de routes
+
+import Accueil from './components/Accueil.js';
+import Connexion from './components/Connexion.js';
+import Inscription from './components/Inscription.js';
+import Profil from './components/Profil.js';
+
+
+function App() {
+  return (
+
+    <div>
+    <div className="App">
+      
+      <Link to="/">Accueil</Link>
+      <Link className="button" to="/connexion">Connexion</Link>
+      <Link className="button" to="/inscription">Inscription</Link>
+      <Link className="button" to="/profil">Profil</Link>
+      
+    </div>
+    <Routes>     
+        {/* <Route exact path="/" component={<Accueil/>} /> */}
+
+        {/* permt la naviguation */}
+        <Route path="/"           element={<Accueil/>}/>
+        
+        <Route path="/connexion/*"  element={<Connexion/>} />
+        <Route path="/inscription"      element={<Inscription/>} />
+        <Route path="/profil"      element={<Profil/>} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
